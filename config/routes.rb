3ca047c55a3964122a78c12cc8a880fa2aa501e2
kazu1029 <imgnt10nxqu05@gmail.com>
2016-09-26 15:22:17 +0900

    new file rooms_controller.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'rooms/index'
+
+  get 'rooms/show'
+
+  get 'rooms/new'
+
+  get 'rooms/create'
+
+  get 'rooms/destroy'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
