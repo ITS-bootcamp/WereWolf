b7d85d5b7e56444d00dd026e9a85d350048d7c05
kazu1029 <imgnt10nxqu05@gmail.com>
2016-09-26 15:25:15 +0900

    delete useless files and setting routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,6 @@
 Rails.application.routes.draw do
-  get 'rooms/index'
-
-  get 'rooms/show'
-
-  get 'rooms/new'
-
-  get 'rooms/create'
-
-  get 'rooms/destroy'
-
+  root 'rooms#index'
+  resources :rooms
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
